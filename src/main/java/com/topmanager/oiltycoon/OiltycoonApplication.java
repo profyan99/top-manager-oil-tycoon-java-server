2eb3718755e274d0736c6565a1333b13d285c53c
Константин Артюшкевич <profyan99@gmail.com>
2018-05-19 23:16:51 +0600

    Init commit

---
+++ src/main/java/com/topmanager/oiltycoon/OiltycoonApplication.java
@@ -0,0 +1,11 @@
+package com.topmanager.oiltycoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OiltycoonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OiltycoonApplication.class, args);
+	}
+}
